89266d223dd2c3879bfe8585506838624e703231
Chukwuemeka Chinemelu <wikiwoo@MacBook-Air.local>
2024-05-01 14:35:59 +0100

    Updated buget page

--- src/components/Buget.tsx
+++ src/components/Buget.tsx
@@ -8,6 +8,15 @@ import {
 import Circle from "../assets/circle.svg";
 import Degree from "../assets/degree.svg";
 import Savings from "../assets/savings.svg";
+import Battery from "../assets/battery.svg";
+import Naira from "../assets/naira.svg";
+import Dot from "../assets/dot.svg";
+import Food from "../assets/food.svg";
+import House from "../assets/house.svg";
+import Report from "../assets/report.svg";
+import Chat from "../assets/chat.svg";
+import Buget from "../assets/buget.svg";
+import Profile from "../assets/vector.svg";
 
 const Budget: React.FC = () => {
     return (
@@ -17,7 +26,7 @@ const Budget: React.FC = () => {
                     <Text className="gap-0">9:41</Text>
                     <Image
                         loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/712db91da54611095d3a7ff8862392fffaa447cfdfc633777df1824c8272898e?"
+                        src={Battery}
                         className="shrink-0 gap-0 aspect-[5.56] w-[67px]"
                     />
                 </Flex>
@@ -27,7 +36,7 @@ const Budget: React.FC = () => {
                 <Flex className="flex gap-2.5 self-start mt-8 text-sm tracking-normal text-gray-500">
                     <Image
                         loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/ec8891895311b238335ec5a9277cd0b03f89c3b4c83dfc1e8e84fe48a27ebb1f?"
+                        src={Naira}
                         className="shrink-0 gap-0 w-5 aspect-square"
                     />
                     <Text className="gap-0">Monthly Budget</Text>
@@ -42,16 +51,12 @@ const Budget: React.FC = () => {
                         <Text className="gap-0 self-start text-gray-500">Last Month</Text>
                         <Flex className="flex flex-col gap-3 text-sky-600">
                             <Text className="gap-0">This Month</Text>
-                            <Image
-                                loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/843bb6d5eafaf531fd1c98856205ef94b6b89fb359b08b58102f07f188a263d4?"
-                                className="gap-0 w-1/2 border-2 border-sky-600 border-solid aspect-[16.67] stroke-[2px] stroke-sky-600"
-                            />
+                            <div className="gap-0 w-1/2 border-2 border-sky-600 border-solid aspect-[16.67] stroke-[2px] stroke-sky-600"></div>
                         </Flex>
                     </Flex>
                     <Image
                         loading="lazy"
-                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/dc068792eb765b29647ceba685f234b7c70bf98bab1277b36bceb5c847f879ff?"
+                        src={Dot}
                         className="shrink-0 gap-0 aspect-[1.89] w-[30px]"
                     />
                 </Flex>
@@ -64,7 +69,6 @@ const Budget: React.FC = () => {
                     <Image
                         loading="lazy"
                         src={Degree}
-                        // className="w-1/2 aspect-[-4]"
                         className="absolute inset-1/4 w-1/2 h-1/2 rounded-full"
                     />
                 </Flex>
@@ -84,7 +88,7 @@ const Budget: React.FC = () => {
                     <Flex className="flex gap-2.5 text-sm tracking-normal">
                         <Image
                             loading="lazy"
-                            src="https://cdn.builder.io/api/v1/image/assets/TEMP/ee0ebef48d93035b444e0ca273256f91add967bcfb924b669aa83c59da2e40e9?"
+                            src={Food}
                             className="shrink-0 gap-0 aspect-square w-[50px]"
                         />
                         <Flex className="flex flex-col gap-0 my-auto">
@@ -126,7 +130,7 @@ const Budget: React.FC = () => {
                         <div className="flex relative flex-col gap-3.5 text-xs text-center whitespace-nowrap font-[450] text-neutral-300">
                             <img
                                 loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/21a130cf38c336a5e1ac76b88a69a12baa7aa50ba4598ab3b6d7ee7f08dfaf5d?"
+                                src={House}
                                 className="gap-0 self-center w-full aspect-[1.54] fill-neutral-300"
                             />
                             <div className="gap-0 mt-3.5 slashed-zero">Home</div>
@@ -135,7 +139,7 @@ const Budget: React.FC = () => {
                         <div className="flex relative flex-col gap-3.5 text-xs text-center whitespace-nowrap font-[450] text-neutral-300">
                             <img
                                 loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/bba2244db0e22a9c598e88a5d4a827e0aa138ea3b4195c4e17eb8631585b772f?"
+                                src={Report}
                                 className="gap-0 self-center w-full aspect-[2] fill-neutral-300"
                             />
                             <div className="gap-0 mt-3.5 slashed-zero">Reports</div>
@@ -144,7 +148,7 @@ const Budget: React.FC = () => {
                         <div className="flex relative flex-col gap-3.5 text-xs text-center text-gray-500 whitespace-nowrap font-[450]">
                             <img
                                 loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/5f87b7d6aab6443578b034cb3d77ca7b35c6f664aef20987c8bbba136ab3280a?"
+                                src={Chat}
                                 className="gap-0 self-center w-full aspect-[0.75] fill-gray-500"
                             />
                             <div className="gap-0 slashed-zero text-gray-500">Chat</div>
@@ -153,7 +157,7 @@ const Budget: React.FC = () => {
                         <div className="flex relative flex-col gap-3.5 text-xs text-center text-gray-500 whitespace-nowrap font-[450]">
                             <img
                                 loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/e8890864cdf822d9f82f3657b825c39881544dc80316c4c3c9ea484276e062bf?"
+                                src={Buget}
                                 className="gap-0 self-center w-5 aspect-[0.56] fill-sky-500"
                             />
                             <div className="gap-0 slashed-zero text-slate-900">Budget</div>
@@ -162,7 +166,7 @@ const Budget: React.FC = () => {
                         <div className="flex relative flex-col gap-3.5 text-xs text-center text-gray-500 whitespace-nowrap font-[450]">
                             <img
                                 loading="lazy"
-                                src="https://cdn.builder.io/api/v1/image/assets/TEMP/efa99544324bb141327404b7ad055643709d6e9fc17e321d8aa8200f6cab660a?"
+                                src={Profile}
                                 className="gap-0 self-center w-full aspect-[1.64] fill-gray-500"
                             />
                             <div className="gap-0 mt-3.5 slashed-zero">Profile</div>
